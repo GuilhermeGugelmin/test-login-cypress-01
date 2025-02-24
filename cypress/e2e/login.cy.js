b7fd10e99d6c15f802be15c2b12f69024a603e30
Guilherme Gugelmin <guilhermegugelmin@gmail.com>
2025-02-24 10:17:40 -0300

    Update

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,6 @@
+import userData from '../fixtures/userData.json'
+
+
 describe('orange HRM Tests', () => {
 
 
@@ -6,23 +9,38 @@ describe('orange HRM Tests', () => {
     passWordFiled: "[name='password']",
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    wrongCredential: "[role='alert']"
+    wrongCredential: "[role='alert']",
+    dashboardGrid: ".orangehrm-dashboard-grid" 
     }
   
   
+    it('login sucess3', () => {
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+      cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+      cy.get(selectorsList.passWordFiled).type(userData.userSucess.password)
+      cy.get(selectorsList.loginButton).click()
+      cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+      cy.get(selectorsList.dashboardGrid)
+    
+    })
 
-  it('login sucess1', () => {
+
+
+  
+
+  it('login sucess2', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameFiled).type('Admin')
+    cy.get(selectorsList.usernameField).type('Admin')
     cy.get(selectorsList.passWordFiled).type('admin123')
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(".oxd-topbar-header-breadcrumb").contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
+  
   })
 
 
 
-  it('login sucess2', () => {
+  it('login sucess1', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get("[name='username']").type('Admin')
     cy.get("[name='password']").type('admin123')
@@ -35,7 +53,42 @@ describe('orange HRM Tests', () => {
 
 
 
-  it('login fail', () => {
+
+
+
+
+
+
+
+  it('login fail3', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passWordFiled).type(userData.userFail.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredential)
+
+  })
+
+
+
+
+
+
+
+  it('login fail2', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type('test')
+    cy.get(selectorsList.passWordFiled).type('test')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredential)
+
+  })
+
+
+
+  
+
+  it('login fail1', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('test')
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('test')
