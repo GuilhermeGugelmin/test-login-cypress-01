918ab1e7b0ad06ac97ae011a75583c88943229b3
Guilherme Gugelmin <guilhermegugelmin@gmail.com>
2025-02-24 14:56:52 -0300

    update2

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -10,9 +10,61 @@ describe('orange HRM Tests', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     wrongCredential: "[role='alert']",
-    dashboardGrid: ".orangehrm-dashboard-grid" 
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    fistNameField: "name-test",
+    lastNameField: "last-name-test",
+    genericField: ".oxd-input",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']", 
+    
+
+
+
+
     }
-  
+
+
+
+    it.only('User Info Update', () => {
+      cy.visit('/auth/login')
+      cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+      cy.get(selectorsList.passWordFiled).type(userData.userSucess.password)
+      cy.get(selectorsList.loginButton).click()
+      cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+      cy.get(selectorsList.dashboardGrid)
+      cy.get(selectorsList.myInfoButton).click()  
+      cy.get('.orangehrm-firstname').clear().type(selectorsList.fistNameField)
+      cy.get('.orangehrm-middlename').clear()
+      cy.get('.orangehrm-lastname').clear().type(selectorsList.lastNameField)
+      cy.get(selectorsList.genericField).eq(4).clear().type('emp-id-tes')  
+      cy.get(selectorsList.genericField).eq(5).clear().type('R$100.000.000')  
+      cy.get(selectorsList.genericField).eq(6).clear().type('$20.000.000')  
+      cy.get(selectorsList.genericField).eq(7).clear().type('2026-10-10')  
+      cy.get(selectorsList.dateCloseButton).click()
+      cy.get(selectorsList.submitButton).eq(0).click()
+      cy.get('body').should('contain', 'Successfully Update')
+      cy.get('.oxd-toast-close')
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+    })
+
+
   
     it('login sucess3', () => {
       cy.visit('/auth/login')
